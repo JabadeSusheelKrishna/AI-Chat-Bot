dc9a23aae14c6b07c5b9b6def75e9e118e9324f2
JabadeSusheelKrishna <susheelkrishna95@gmail.com>
2024-05-26 07:29:19 +0530

    Added Gemini APIs Access

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # AI-Chat-Bot
 This is an AI Chat Bot Developed Using Gemini AI Pro.
+
+### Instructions to run
+- Clone the repository by running the following command : 
+```
+git clone https://github.com/JabadeSusheelKrishna/AI-Chat-Bot.git
+```
+- Now go the the Local Repository directory by running the following command :
+```
+cd AI-Chat-Bot
+```
+- Now Install the Gemini Dependencies and Flask Modules by running the followeing commands :
+```
+pip3 install Flask
+pip3 install -U google-generativeai
+```
+- Now we need to run the server. to do that try following command : 
+```
+python3 server.py
+```
+- Now We are completely ready with the backend. After running the server, you should be able to see a link which looks like `http://127.0.0.XXXX`. Just go to the Browser and search this URL.
+- Thats it, you should be now able to start chatting with Gemini.
+
+### Contributions :
+- Fork my repository
+- if you want modify the UI part, you can directly go to `templates` directory and make modifications there.
+- if you want to modify the Server part, you can change `server.py`. also each time when you change it, make sure that the AI remembers its previous chat in case of same user and forgets when user gets switched.
---
+++ server.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, jsonify
+import google.generativeai as genai
+import time
+
+app = Flask(__name__)
+
+# Sample messages to start with
+messages = []
+
+# Initializing Username
+UserName = "uuu"
+
+genai.configure(api_key='AIzaSyDn2cA_MQJ4_W0OrsVYtOHY8FqUt3atPGU')
+model = genai.GenerativeModel(model_name="gemini-pro")
+chat = model.start_chat()
+
+def Gemini_Answer(user_msg):
+    response = chat.send_message(user_msg)
+    return response.text
+
+@app.route('/')
+def index():
+    return render_template('index.html', messages=messages)
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    global messages
+    global UserName  # Ensure UserName is global
+    global chat
+
+    username = request.form['username']
+    text = request.form['text']
+    timestamp = f"{time.localtime().tm_hour}:{time.localtime().tm_min}"
+
+    if UserName != username:
+        UserName = username
+        messages = []
+        chat = model.start_chat()
+
+    # Add the user message to the list
+    new_message = {"username": username, "timestamp": timestamp, "text": text}
+    messages.append(new_message)
+
+    # Print message to terminal
+    print(f'{username}: {text}')
+    
+    random_message = Gemini_Answer(text)
+
+    random_message_data = {"username": "Gemini", "timestamp": timestamp, "text": random_message}
+    messages.append(random_message_data)
+
+    return jsonify(messages=messages)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
